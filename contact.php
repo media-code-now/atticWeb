447a023122b38e4f384734325e6d91cdcd848a43
media-code-now <mediacodenow@gmail.com>
2022-05-26 21:24:11 -0700

    main

---
+++ contact.php
@@ -0,0 +1,51 @@
+<?php
+
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+    if (empty($phone)) {
+        $errors[] = 'Phone is empty';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+
+    if (empty($errors)) {
+        $toEmail = 'erniepanza@gmail.com';
+        // $toEmail = 'akatziri@gmail.com';
+        $emailSubject = 'New email from your contant form';
+        $headers = "From: " . strip_tags($_POST['email']) . "\r\n";
+        $headers .= "Reply-To: ". strip_tags($_POST['email']) . "\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+        $bodyParagraphs = ["Name: {$name}" ,"Phone: {$phone}","Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: contact.html');
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
+}
